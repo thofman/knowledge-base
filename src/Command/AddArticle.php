d1265f81107ab87e55e09fea1e25e8f3664a32e0
Tim Hofman <10279280+thofman@users.noreply.github.com>
2026-02-06 10:40:34 +0100

    Add possible commit message for adding an article

--- src/Command/AddArticle.php
+++ src/Command/AddArticle.php
@@ -124,6 +124,9 @@ final class AddArticle extends BaseCommand
         $output->writeln($fileContents5);
         file_put_contents($directoryAndFileNameWithExtension, $fileContents5);
         $output->writeln(sprintf('Article with .md-file "%s" is added', $fileNameWithExtension));
+        $output->writeln(
+            sprintf('You can use this as commit message: "%s"`', sprintf('Add article: %s - %s', $author, $title))
+        );
         return Command::SUCCESS;
     }
 
