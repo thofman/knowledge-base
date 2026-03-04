97fd6ff7d6aa150abfd0beddc860c194e0f63aa5
Tim Hofman <10279280+thofman@users.noreply.github.com>
2026-03-04 21:09:25 +0100

    Optimize imports

--- src/Domain/Question/Validation/Validator.php
+++ src/Domain/Question/Validation/Validator.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace thofman\KnowledgeBase\Domain\Question\Validation;
 
-use Symfony\Component\Console\Question\Question;
-
 interface Validator
 {
     public function validate(string $value): ValidationResult;
