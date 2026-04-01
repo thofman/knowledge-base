37b053b47a1ff23f83b25c66a80a79babbf8d291
Tim Hofman <10279280+thofman@users.noreply.github.com>
2026-04-01 20:33:28 +0200

    feat(index): replace hardcoded filename by IndexMarkdownFile

--- src/Infrastructure/MarkdownFile/GlobMarkdownFileRepository.php
+++ src/Infrastructure/MarkdownFile/GlobMarkdownFileRepository.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 namespace thofman\KnowledgeBase\Infrastructure\MarkdownFile;
 
 use SplFileInfo;
+use thofman\KnowledgeBase\Domain\MarkdownFile\Index\IndexMarkdownFile;
 use thofman\KnowledgeBase\Domain\MarkdownFile\MarkdownFile;
 use thofman\KnowledgeBase\Domain\MarkdownFile\MarkdownFileCollection;
 use thofman\KnowledgeBase\Domain\MarkdownFile\MarkdownFileRepository;
 use thofman\KnowledgeBase\Domain\MarkdownFile\MarkdownFilesDirectory;
 
-final class GlobMarkdownFileRepository implements MarkdownFileRepository
+final readonly class GlobMarkdownFileRepository implements MarkdownFileRepository
 {
+    public function __construct(
+        private IndexMarkdownFile $indexMarkdownFile,
+    ) {
+    }
+
     public function getMarkdownFileCollection(MarkdownFilesDirectory $markdownFilesDirectory): MarkdownFileCollection
     {
         $allMdFiles = glob($markdownFilesDirectory->getPathname() . '/*.md');
         $mdFilesWithoutIndex = array_filter(
             $allMdFiles,
-            static fn(string $file): bool => !str_ends_with($file, '_index.md'),
+            fn(string $file): bool => !str_ends_with($file, $this->indexMarkdownFile->getFilename()),
         );
         return new MarkdownFileCollection(
             ...array_map(
